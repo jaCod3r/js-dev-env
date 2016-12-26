70acdb241182a227519cec315e8f350086231925
Juber Ahmed <a.juber@hotmail.com>
2016-12-26 02:38:35 -0500

    Added ESLint comments

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 
-console.log(chalk.cyan.underline("Starting App in Dev Mode..."))
+console.log(chalk.cyan.underline("Starting App in Dev Mode...")) //eslint-disable-line no-console
